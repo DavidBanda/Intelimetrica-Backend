6fec416b407a3fca2dced807b5a04f62d60c5c5b
David Banda <davidbandag96@gmail.com>
2020-09-19 16:24:05 -0600

    Basic project structure configuration
    We create a python package called "melp_backend" with a file inside called
    config.py where we add a Class with the secret_key value and the URI value
    to the DB and also we add a file outside of the package called app.run to run
    the application.

---
+++ melp_backend/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from melp_backend.config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
---
+++ melp_backend/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SECRET_KEY = 'e7e4b44cfa377d362eae765337be7361872b62bb7fc0bf2fea25f4d88222291f'
+    SQLALCHEMY_DATABASE_URI = f'mysql://root:DAVIDAAROn15@localhost/melp_backend'
