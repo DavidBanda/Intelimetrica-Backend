03bef3f37dcb8263de9fe54e181210868ba0b7c5
David Banda <davidbandag96@gmail.com>
2020-09-19 17:59:18 -0600

    Create app restaurants with Flask Blueprint object

--- melp_backend/restaurants/routes.py
+++ melp_backend/restaurants/routes.py
@@ -2,3 +2,6 @@ from flask import request, jsonify, Blueprint
 from melp_backend import db
 from melp_backend.restaurants.models import Restaurant, restaurant_schema, restaurants_schema
 
+restaurants = Blueprint('restaurants', __name__)
+
+
