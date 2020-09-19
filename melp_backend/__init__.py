30452861dd0a15e084123ae0ecf183be9a1ee4eb
David Banda <davidbandag96@gmail.com>
2020-09-19 16:39:27 -0600

    add flask-marshmallow

--- melp_backend/__init__.py
+++ melp_backend/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_marshmallow import Marshmallow
 from melp_backend.config import Config
 
 app = Flask(__name__)
@@ -10,3 +11,4 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
