91428ad55036f068bcaa2ac01298b77ec8d263ef
David Banda <davidbandag96@gmail.com>
2020-09-19 21:30:32 -0600

    Modify id, rating, site and phone columns in Restaurant model

--- melp_backend/restaurants/models.py
+++ melp_backend/restaurants/models.py
@@ -1,21 +1,23 @@
 from melp_backend import db, ma
+import uuid
 
 
 # Restaurant Class/Model
 class Restaurant(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    rating = db.Column(db.Integer, nullable=False, default=5)
+    id = db.Column(db.String(45), primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    site = db.Column(db.String(150), unique=True)
+    site = db.Column(db.String(150))
     email = db.Column(db.String(120), nullable=False, unique=True)
-    phone = db.Column(db.String(10), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
     street = db.Column(db.String(100), nullable=False)
     city = db.Column(db.String(50), nullable=False)
     state = db.Column(db.String(50), nullable=False)
     lat = db.Column(db.Float)
     lng = db.Column(db.Float)
 
-    def __init__(self, rating, name, site, email, phone, street, city, state, lat=None, lng=None):
+    def __init__(self, rating, name, site, email, phone, street, city, state, lat=None, lng=None, id=uuid.uuid1()):
+        self.id = id
         self.rating = rating
         self.name = name
         self.site = site
