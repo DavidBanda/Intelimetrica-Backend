8bf3ae8a50f1083372a9b800fbe5076e18a3815b
David Banda <davidbandag96@gmail.com>
2020-09-20 15:58:40 -0600

    Change the SQLALCHEMY_DATABASE_URI

--- melp_backend/config.py
+++ melp_backend/config.py
@@ -1,4 +1,5 @@
 class Config:
     SECRET_KEY = 'e7e4b44cfa377d362eae765337be7361872b62bb7fc0bf2fea25f4d88222291f'
-    SQLALCHEMY_DATABASE_URI = f'mysql://root:DAVIDAAROn15.@localhost/melp_backend'
+    # SQLALCHEMY_DATABASE_URI = f'mysql://root:DAVIDAAROn15.@localhost/melp_backend'
+    SQLALCHEMY_DATABASE_URI = f'mysql://bff93b7b70928c:6249e907@us-cdbr-east-02.cleardb.com/heroku_0c4d9807e7cbe14'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
