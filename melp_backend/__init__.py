07cd063b7f07140ce2acca1e05e514f0213cbc9a
David Banda <davidbandag96@gmail.com>
2020-09-19 22:04:23 -0600

    Remove bcrypt and login manager from melp_backend/__init__.py

--- melp_backend/__init__.py
+++ melp_backend/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from melp_backend.config import Config
 from flask_marshmallow import Marshmallow
 
@@ -9,9 +7,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 ma = Marshmallow(app)
-login_manager = LoginManager(app)
 
 from melp_backend.restaurants.routes import restaurants
 
